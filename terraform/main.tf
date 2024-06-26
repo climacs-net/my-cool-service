c1dd14e23aa54c1cdfaf0e85f9540a8631a1b1d8
Gary Climacosa <gary.climacosa@aa.com>
2024-06-26 23:21:42 +0200

    update ansible inventory and playbook, tf main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,36 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
+}
+
+data "aws_key_pair" "existing" {
+  key_name = "terraform_aws_key"
+  filter {
+    name   = "key-name"
+    values = ["terraform_aws_key"]
+  }
+  count = 0
 }
 
 resource "aws_key_pair" "deployer" {
-  count = length(data.aws_key_pair.existing.key_name) == 0 ? 1 : 0
+  count      = length(data.aws_key_pair.existing) == 0 ? 1 : 0
   key_name   = "terraform_aws_key"
   public_key = file("~/.ssh/id_ed25519.pub")
 }
 
-resource "aws_security_group" "my_cool_service_sg" {
-  name        = "my_cool_service_sg"
-  description = "Allow all inbound traffic"
-  vpc_id      = "YOUR_VPC_ID"
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+locals {
+  key_name = length(data.aws_key_pair.existing) == 0 ? aws_key_pair.deployer[0].key_name : "terraform_aws_key"
 }
 
 resource "aws_instance" "my_cool_service" {
-  ami           = "ami-04505e74c0741db8d" # Ubuntu 22.04 LTS AMI
-  instance_type = "t3.medium"
-  key_name      = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.my_cool_service_sg.name]
+  ami               = "ami-06f59e43b31a49ecc" # Ubuntu Server 24.04 LTS (HVM), update to the correct AMI for your region
+  instance_type     = "t3.medium" # Specify the desired instance type
+  key_name          = local.key_name
+  availability_zone = "us-east-1b" # Ensure this is a valid availability zone
+
+  root_block_device {
+    volume_size = 25 # Size in GB
+    volume_type = "gp2" # General Purpose SSD
+  }
 
   user_data = <<-EOF
               #!/bin/bash
@@ -48,18 +46,19 @@ resource "aws_instance" "my_cool_service" {
   }
 }
 
-resource "aws_eip" "my_cool_service_eip" {
+resource "aws_eip" "elastic_ip" {
   instance = aws_instance.my_cool_service.id
+  vpc      = true
 }
 
-resource "aws_route53_record" "my_cool_service" {
-  zone_id = "Z3RUW5P7TDNES4"
-  name    = "my-cool-service.climacs.net"
+resource "aws_route53_record" "fqdn" {
+  zone_id = "Z3RUW5P7TDNES4" # Replace with your actual Route 53 hosted zone ID
+  name    = "my-cool-service.climacs.net" # Replace with your desired domain name
   type    = "A"
   ttl     = "300"
-  records = [aws_eip.my_cool_service_eip.public_ip]
+  records = [aws_eip.elastic_ip.public_ip]
 }
 
 output "instance_ip" {
-  value = aws_instance.my_cool_service.public_ip
+  value = aws_eip.elastic_ip.public_ip
 }
